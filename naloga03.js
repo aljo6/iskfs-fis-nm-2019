63b1ff6ef3c5c8dcea986d729ad1d7f604756c69
Aljaž Bukovec <47948678+aljo6@users.noreply.github.com>
2019-03-21 16:21:04 +0100

    Add files via upload
    naknadno dodana naloga 3

--- naloga03.js
+++ naloga03.js
@@ -14,7 +14,7 @@ var board = new firmata.Board("/dev/ttyACM0", function(){
 });
 
 function handler(req, res) {
-    fs.readFile(__dirname + "/primer07.html",
+    fs.readFile(__dirname + "/naloga03.html",
     function (err, data) {
         if (err) {
             res.writeHead(500, {"Content-Type": "text/plain"});
@@ -40,37 +40,18 @@ io.sockets.on("connection", function(socket) {
     
 }); // konec "sockets.on connection"
 
-var timeout = false;
-var zadnjaPoslana = null;
-var zadnjaVrednost = null;
-
-board.digitalRead(2, function(value) { // digitalno branje se dogodi večkrat, ob spremembi stanja iz 0->1 ali 1->0
-    if (timeout !== false) { // če se je timeout spodaj pričel, (ob nestabilnem vhodu, npr. 0 1 0 1) ga biršemo
-	   clearTimeout(timeout); // brišemo timeout dokler ni digitalni vhod stabilen, i.e. timeout = false
-	   console.log("Timeout je postavljen na false");
+board.digitalRead(2, function(value) {
+    if (value == 0) {
+        console.log("LED izklopljena");
+        board.digitalWrite(13, board.LOW);
+        pošljiVrednostPrekoVtičnika(0);
     }
-timeout = setTimeout(function() { // ta del kode se bo izvedel po 50 ms; če se vmes dogodi sprememba stanja, ga gornja koda izbriše
-// zgornja vrstica pomeni timeout = true
-    console.log("Timeout je postavljen na true");
-    timeout = false;
-    if (zadnjaVrednost != zadnjaPoslana) { // to send only on value change
-    	if (value == 0) {
-            board.digitalWrite(13, board.LOW);
-            console.log("Vrednost = 0, LED izklopljena");
-            pošljiVrednostPrekoVtičnika(0);
-        }
-        else if (value == 1) {
-            board.digitalWrite(13, board.HIGH);
-            console.log("Vrednost = 1, LED prižgana");
-            pošljiVrednostPrekoVtičnika(1);
-        }
-
+    if (value == 1) {
+        console.log("LED vklopljena");
+        board.digitalWrite(13, board.HIGH);
+        pošljiVrednostPrekoVtičnika(1);
     }
-
-    zadnjaPoslana = zadnjaVrednost;
-}, 50); // izvedemo po 50ms
-            
-zadnjaVrednost = value; // ta vrednost se prebere iz nožice 2 večkrat na sekundo.
+    
 }); // konec "board.digitalRead"
 
 }); // konec "board.on ready"
